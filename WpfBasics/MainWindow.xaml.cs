104e6ceafa0b7acb4bea0b44dfa59fd3c074113f
Neskaya <39946966+n8water@users.noreply.github.com>
2019-04-27 13:45:21 +0200

    Tutorial 1 fertig

--- WpfBasics/MainWindow.xaml.cs
+++ WpfBasics/MainWindow.xaml.cs
@@ -23,6 +23,46 @@ namespace WpfBasics
         public MainWindow()
         {
             InitializeComponent();
+        } 
+
+        private void ApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"The description is: {this.DescriptionText.Text}");
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.chbWeld.IsChecked = this.chbAssembly.IsChecked = this.chbDrill.IsChecked =
+                this.chbFold.IsChecked = this.chbLaser.IsChecked = this.chbLathe.IsChecked =
+                this.chbPlasma.IsChecked = this.chbPurchase.IsChecked = chbRoll.IsChecked =
+                this.chbSaw.IsChecked = false;
+        }        
+
+        private void Chb_Checked(object sender, RoutedEventArgs e)
+        {
+            this.LengthText.Text += ((CheckBox)sender).Content;
+            //this.LengthText.Text += (string)((CheckBox)sender).Content;
+        }
+
+        private void FinishDropdown_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(this.NoteText == null)
+                return;
+
+            var combo = (ComboBox)sender;
+            var value = ((ComboBoxItem)(combo).SelectedValue);
+
+            this.NoteText.Text = (string)value.Content;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            FinishDropdown_SelectionChanged(this.FinishDropdown, null);
+        }
+
+        private void SupplierNameText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.MassText.Text = ((TextBox)SupplierNameText).Text;
         }
     }
 }
