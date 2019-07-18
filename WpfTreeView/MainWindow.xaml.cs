150fa72ac4da4866c248aa5e82f152e0759150e1
Neskaya <39946966+n8water@users.noreply.github.com>
2019-07-18 22:25:14 +0200

    16 minutes of the video

--- WpfTreeView/MainWindow.xaml.cs
+++ WpfTreeView/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,33 @@ namespace WpfTreeView
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Ctor
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+
         }
+
+        #endregion
+
+        #region On Loaded
+        /// <summary>
+        /// Wen the application first opens
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach(var drive in  Directory.GetLogicalDrives())
+            {
+                var item = new TreeViewItem();
+                 FolderView.Items.Add(drive);
+            }
+        }
+
+        #endregion
     }
 }
